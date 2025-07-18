eb3ae3cc1ec005d04cdae8f165751b1fc9c1482b
Bart Massey <bart.massey@gmail.com>
2025-07-18 16:40:49 -0700

    improved prompt and prompt reading

--- src/generate.rs
+++ src/generate.rs
@@ -2,7 +2,14 @@ pub fn run() -> (crate::puzzle::Square, crate::puzzle::Square) {
     let puzzles = crate::puzzle::load();
     let answers = crate::puzzle::new(&puzzles);
     let client = reqwest::blocking::Client::new();
-    let prompt = std::fs::read_to_string("prompt.txt").expect("Failed to read prompt.txt.");
+    let mut prompt = std::fs::read_to_string("prompt.txt")
+        .expect("Failed to read prompt.txt.");
+    prompt = prompt.replace('\n', " ");
+    let prompt: String = prompt
+        .trim()
+        .chars()
+        .filter(|&c| c.is_ascii() && !c.is_ascii_control())
+        .collect();
 
     let mut across: Vec<String> = vec![];
     let mut down: Vec<String> = vec![];
